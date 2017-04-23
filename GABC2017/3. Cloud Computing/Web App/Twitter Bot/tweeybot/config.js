e96798a099b1aa5dbd7a5ec2e5139d15213505fd
Binod Jung Bogati <m1s1ng@outlook.com>
2017-04-23 08:41:08 +0545

    GABC commit

---
+++ GABC2017/3. Cloud Computing/Web App/Twitter Bot/tweeybot/config.js
@@ -0,0 +1,12 @@
+// Enter your credentials from Twitter App - https://apps.twitter.com
+
+module.exports = {
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
+}
+
+
+
+
